3ac4ca6af0397e22a9b71add30a3a998f9b2b1c5
Uttam Negi <piyush.negi@kalvium.community>
2023-04-27 11:34:41 +0530

    inor bug fixes

--- IDE/onlineIde/utils.py
+++ IDE/onlineIde/utils.py
@@ -4,29 +4,30 @@ import django
 django.setup()
 from .models import SubTab
 
+
 def create_code_file(code, language):
     file_name = str(uuid.uuid4()) + "." + language
     with open("code/" + file_name, "w") as f:
         f.write(code)
     return file_name
 
-def execute_code(file_name, language, sub_id):
-    sub = SubTab.objects.get(pk=sub_id)
-    if language == "cpp":
-        result = subprocess.run(["g++", "code/" + file_name], stdout=subprocess.PIPE) #Compiling the code
-        if(result.returncode != 0):
-            #Compilation Error
-            sub.status = 'E'
-            sub.save()
-            return
-        
-        result = subprocess.run(["a.exe"], stdout=subprocess.PIPE)
-        if(result.returncode != 0):
-            #Runtime Error
-            sub.status = 'E'
-            sub.save()
-            return
 
-        sub.output = result.stdout.decode("utf-8")
-        sub.status = 'S'
-        sub.save()
+def execute_code(file_name, language, submission_id):
+    submission = SubTab.objects.get(pk=submission_id)
+    if language == "cpp":
+        # g++ xyz.cpp
+        result = subprocess.run(["g++", "code/" + file_name], stdout=subprocess.PIPE)
+        if result.returncode != 0:
+            # Compile time error
+            submission.status = 'E'
+            submission.save()
+            return
+        result = subprocess.run(["a.exe"], stdout=subprocess.PIPE)
+        if result.returncode != 0:
+            submission.status = 'E'
+            submission.save()
+            return
+        submission.output = result.stdout.decode("utf-8")
+        submission.status = 'S'
+        submission.save()
+
